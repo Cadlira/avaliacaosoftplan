ee5450fe016ed952d279221e4ac6ace4e65e373f
Leonardo Lira <cadlira@gmail.com>
2019-09-12 01:14:15 -0300

    Adicionando tests suites

--- src/test/java/br/com/softplan/avaliacao/AvaliacaoSoftplanApplicationTests.java
+++ src/test/java/br/com/softplan/avaliacao/AvaliacaoSoftplanApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import br.com.softplan.avaliacao.exercicio1.Exercicio1TestSuite;
+import br.com.softplan.avaliacao.exercicio2.Exercicio2TestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ Exercicio1TestSuite.class })
+@Suite.SuiteClasses({ Exercicio1TestSuite.class, Exercicio2TestSuite.class })
 public class AvaliacaoSoftplanApplicationTests {}
---
+++ src/test/java/br/com/softplan/avaliacao/exercicio2/Exercicio2TestSuite.java
@@ -0,0 +1,12 @@
+package br.com.softplan.avaliacao.exercicio2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import br.com.softplan.avaliacao.exercicio2.model.ModelTestSuite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ModelTestSuite.class})
+public class Exercicio2TestSuite {
+
+}
